1cb7c44a9cbeb4169fc4d0d2a5f29223370e5eb4
Andy <88871722+NikAndSer@users.noreply.github.com>
2023-05-02 08:38:00 +0700

    Add random quote generator

---
+++ quotes.js
@@ -0,0 +1,184 @@
+   const quotes = [ 
+   {
+    
+	quote: "Life isn’t about getting and having, it’s about giving and being.",
+    
+	author: "Kevin Kruse"
+    },
+    {
+    
+	quote: "The best way to find yourself is to lose yourself in the service of others.",
+    
+	author: "Mahatma Gandhi"
+    },
+    {
+    
+	quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    
+	author: "Nelson Mandela"
+    },
+    {
+    
+	quote: "Happiness is not something ready made. It comes from your own actions.",
+    
+	author: "Dalai Lama"
+    },
+    {
+    
+	quote: "Believe you can and you're halfway there.",
+    
+	author: "Theodore Roosevelt"
+    },
+    {
+    
+	quote: "We can't help everyone, but everyone can help someone.",
+    
+	author: "Ronald Reagan"
+    },
+    {
+    
+	quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    
+	author: "Winston Churchill"
+    },
+    {
+    
+	quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    
+	author: "Thomas Edison"
+    },
+    {
+    
+	quote: "The only way to do great work is to love what you do.",
+    
+	author: "Steve Jobs"
+    },
+    {
+    
+	quote: "Strive not to be a success, but rather to be of value.",
+    
+	author: "Albert Einstein"
+    },
+    {
+    
+	quote: "You miss 100% of the shots you don't take.",
+    
+	author: "Wayne Gretzky"
+    },
+    {
+    
+	quote: "It does not matter how slowly you go as long as you do not stop.",
+    
+	author: "Confucius"
+    },
+    {
+    
+	quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    
+	author: "Nelson Mandela"
+    },
+    {
+    
+	quote: "If you want to live a happy life, tie it to a goal, not to people or things.",
+    
+	author: "Albert Einstein"
+    },
+    {
+    
+	quote: "I attribute my success to this: I never gave or took any excuse.",
+    
+	author: "Florence Nightingale"
+    },
+    {
+    
+	quote: "Life is 10% what happens to us and 90% how we react to it.",
+    
+	author: "Charles R. Swindoll"
+    },
+    {
+    
+	quote: "The only true wisdom is in knowing you know nothing.",
+    
+	author: "Socrates"
+    },
+    {
+    
+	quote: "The best revenge is massive success.",
+    
+	author: "Frank Sinatra"
+    },
+    {
+    
+	quote: "In three words I can sum up everything I've learned about life: it goes on.",
+    
+	author: "Robert Frost"
+    },
+    {
+    
+	quote: "The two most important days in your life are the day you are born and the day you find out why.",
+    
+	author: "Mark Twain"
+    },
+    {
+    
+	quote: "Don't watch the clock; do what it does. Keep going.",
+    
+	author: "Sam Levenson"
+    },
+    {
+    
+	quote: "You can't build a reputation on what you are going to do.",
+    
+	author: "Henry Ford"
+    },
+    {
+    
+	quote: "Be the change that you wish to see in the world.",
+	author: "Mahatma Gandhi"
+    },
+    {
+    
+	quote: "I would rather die of passion than of boredom.",
+	author: "Vincent van Gogh"
+    },
+    {
+        quote: "All we have to decide is what to do with the time that is given us.",
+        author: "Gandalf the Grey"
+    },
+    {
+        quote: "Even the darkest night will end and the sun will rise.",
+        author: "Victor Hugo (Les Misérables)"
+    },
+    {
+        quote: "It does not do to dwell on dreams and forget to live.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "Words are, in my not-so-humble opinion, our most inexhaustible source of magic. Capable of both inflicting injury, and remedying it.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "I am what I am an’ I’m not ashamed. 'Never be ashamed,' my ol’ dad used ter say, 'there’s some who’ll hold it against you, but they’re not worth botherin’ with.'",
+        author: "Rubeus Hagrid"
+    },
+    {
+        quote: "Happiness can be found even in the darkest of times if only one remembers to turn on the light.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "I’m a wizard, Harry.",
+        author: "Ron Weasley"
+    },
+    {
+        quote: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+        author: "Albus Dumbledore"
+    },
+    {
+        quote: "There’s some good in this world, Mr. Frodo… and it’s worth fighting for.",
+        author: "Samwise Gamgee"
+    },
+    {
+        quote: "Don't panic!",
+        author: "Douglas Adams"
+    }
+   ]
